512038c15b5e10eb9bc8bb2455a6b3be2ef3ecc8
Leonard Speiser <leonard@horizon3.net>
2024-07-01 04:06:21 +0000

    finally working!

--- public/script.js
+++ public/script.js
@@ -1,149 +1,149 @@
 const socket = io();
 
-const startDiv = document.getElementById('start');
-const createGameDiv = document.getElementById('createGame');
-const joinGameDiv = document.getElementById('joinGame');
-const waitingRoomDiv = document.getElementById('waitingRoom');
-const gameDiv = document.getElementById('game');
-const resultDiv = document.getElementById('result');
+const startDiv = document.getElementById("start");
+const createGameDiv = document.getElementById("createGame");
+const joinGameDiv = document.getElementById("joinGame");
+const waitingRoomDiv = document.getElementById("waitingRoom");
+const gameDiv = document.getElementById("game");
+const resultDiv = document.getElementById("result");
 
-const createGameButton = document.getElementById('createGameButton');
-const joinGameButton = document.getElementById('joinGameButton');
-const submitNamesButton = document.getElementById('submitNamesButton');
-const joinGameSubmitButton = document.getElementById('joinGameSubmitButton');
-const startGameButton = document.getElementById('startGameButton');
+const createGameButton = document.getElementById("createGameButton");
+const joinGameButton = document.getElementById("joinGameButton");
+const submitNamesButton = document.getElementById("submitNamesButton");
+const joinGameSubmitButton = document.getElementById("joinGameSubmitButton");
+const startGameButton = document.getElementById("startGameButton");
 
-const namesInput = document.getElementById('namesInput');
-const gameCodeInput = document.getElementById('gameCodeInput');
-const waitingRoomGameCode = document.getElementById('waitingRoomGameCode');
-const playersList = document.getElementById('playersList');
+const namesInput = document.getElementById("namesInput");
+const gameCodeInput = document.getElementById("gameCodeInput");
+const waitingRoomGameCode = document.getElementById("waitingRoomGameCode");
+const playersList = document.getElementById("playersList");
 
-const topLabel = document.getElementById('topLabel');
-const bottomLabel = document.getElementById('bottomLabel');
-const sortableList = document.getElementById('sortable');
-const lockButton = document.getElementById('lockButton');
+const topLabel = document.getElementById("topLabel");
+const bottomLabel = document.getElementById("bottomLabel");
+const sortableList = document.getElementById("sortable");
+const lockButton = document.getElementById("lockButton");
 
-const resultText = document.getElementById('resultText');
-const finalResultsList = document.getElementById('finalResults');
-const nextButton = document.getElementById('nextButton');
+const resultText = document.getElementById("resultText");
+const finalResultsList = document.getElementById("finalResults");
+const nextButton = document.getElementById("nextButton");
 
 let currentRankingIndex = 0;
 let rankings = [];
-let userId = localStorage.getItem('userId');
+let userId = localStorage.getItem("userId");
 
 const generateGameCode = () => Math.random().toString(36).substring(2, 7).toUpperCase();
 
-const storedGameCode = localStorage.getItem('gameCode');
+const storedGameCode = localStorage.getItem("gameCode");
 
 if (storedGameCode && userId) {
     console.log(`[script.js] Found stored game code: ${storedGameCode}, UUID: ${userId}`);
-    socket.emit('checkGameExists', { gameCode: storedGameCode, userId });
+    socket.emit("checkGameExists", { gameCode: storedGameCode, userId });
 } else {
-    startDiv.style.display = 'flex';
+    startDiv.style.display = "flex";
 }
 
-socket.on('gameExists', ({ exists }) => {
-    const isCreator = localStorage.getItem('isCreator') === 'true';
+socket.on("gameExists", ({ exists }) => {
+    const isCreator = localStorage.getItem("isCreator") === "true";
     console.log(`[script.js/socket.on(gameExists)] User is creator: ${isCreator}`);
-    console.log(`[script.js/socket.on(gameExists)] Start button will ${exists && isCreator ? 'be shown' : 'not be shown'}`);
-    startGameButton.style.display = exists && isCreator ? 'block' : 'none';
+    console.log(`[script.js/socket.on(gameExists)] Start button will ${exists && isCreator ? "be shown" : "not be shown"}`);
+    startGameButton.style.display = exists && isCreator ? "block" : "none";
     if (!exists) {
-        localStorage.removeItem('isCreator');
-        console.log('[script.js/socket.on(gameExists)] Game does not exist. Creator flag removed.');
-        startDiv.style.display = 'flex';
+        localStorage.removeItem("isCreator");
+        console.log("[script.js/socket.on(gameExists)] Game does not exist. Creator flag removed.");
+        startDiv.style.display = "flex";
     }
 });
 
-createGameButton.addEventListener('click', () => {
-    startDiv.style.display = 'none';
-    createGameDiv.style.display = 'flex';
+createGameButton.addEventListener("click", () => {
+    startDiv.style.display = "none";
+    createGameDiv.style.display = "flex";
 });
 
-submitNamesButton.addEventListener('click', () => {
-    const names = namesInput.value.split(/[\n,]+/).map(name => name.trim()).filter(name => name);
+submitNamesButton.addEventListener("click", () => {
+    const names = namesInput.value.split(/[\n,]+/).map((name) => name.trim()).filter((name) => name);
     if (names.length > 0) {
         const gameCode = generateGameCode();
         userId = uuid.v4();
-        localStorage.setItem('gameCode', gameCode);
-        localStorage.setItem('userId', userId);
-        localStorage.setItem('isCreator', 'true');
+        localStorage.setItem("gameCode", gameCode);
+        localStorage.setItem("userId", userId);
+        localStorage.setItem("isCreator", "true");
         console.log(`[script.js/submitNamesButton.click] Game code: ${gameCode}, Names: ${names}, UUID: ${userId}, Creator: true`);
-        socket.emit('createGame', { gameCode, names, userId });
-        createGameDiv.style.display = 'none';
-        waitingRoomDiv.style.display = 'flex';
+        socket.emit("createGame", { gameCode, names, userId });
+        createGameDiv.style.display = "none";
+        waitingRoomDiv.style.display = "flex";
         waitingRoomGameCode.textContent = gameCode;
     } else {
-        console.log('[script.js/submitNamesButton.click] Names input is empty');
+        console.log("[script.js/submitNamesButton.click] Names input is empty");
     }
 });
 
-joinGameButton.addEventListener('click', () => {
-    startDiv.style.display = 'none';
-    joinGameDiv.style.display = 'flex';
+joinGameButton.addEventListener("click", () => {
+    startDiv.style.display = "none";
+    joinGameDiv.style.display = "flex";
 });
 
-joinGameSubmitButton.addEventListener('click', () => {
+joinGameSubmitButton.addEventListener("click", () => {
     const gameCode = gameCodeInput.value.trim();
     userId = uuid.v4();
 
     if (gameCode) {
         console.log(`[script.js/joinGameSubmitButton.click] Game code: ${gameCode}, UUID: ${userId}, Creator: false`);
-        socket.emit('joinGame', { gameCode, userId });
-        localStorage.setItem('gameCode', gameCode);
-        localStorage.setItem('userId', userId);
-        localStorage.removeItem('isCreator');
-        console.log('[script.js/joinGameSubmitButton.click] Creator flag removed');
-        joinGameDiv.style.display = 'none';
-        waitingRoomDiv.style.display = 'flex';
+        socket.emit("joinGame", { gameCode, userId });
+        localStorage.setItem("gameCode", gameCode);
+        localStorage.setItem("userId", userId);
+        localStorage.removeItem("isCreator");
+        console.log("[script.js/joinGameSubmitButton.click] Creator flag removed");
+        joinGameDiv.style.display = "none";
+        waitingRoomDiv.style.display = "flex";
         waitingRoomGameCode.textContent = gameCode;
     } else {
-        console.log('[script.js/joinGameSubmitButton.click] Game code input is empty');
+        console.log("[script.js/joinGameSubmitButton.click] Game code input is empty");
     }
 });
 
-socket.on('gameJoined', (data) => {
+socket.on("gameJoined", (data) => {
     console.log(`[script.js/socket.on('gameJoined')] Joined game: ${data.gameCode}`);
     updatePlayersList(data.names);
-    const isCreator = localStorage.getItem('isCreator') === 'true';
+    const isCreator = localStorage.getItem("isCreator") === "true";
     console.log(`[script.js/socket.on('gameJoined')] User is creator: ${isCreator}`);
-    startGameButton.style.display = isCreator ? 'block' : 'none';
-    console.log(`[script.js/socket.on('gameJoined')] Start button will ${isCreator ? 'be shown' : 'not be shown'}`);
+    startGameButton.style.display = isCreator ? "block" : "none";
+    console.log(`[script.js/socket.on('gameJoined')] Start button will ${isCreator ? "be shown" : "not be shown"}`);
 });
 
-socket.on('playerJoined', (data) => {
+socket.on("playerJoined", (data) => {
     console.log(`[script.js/socket.on('playerJoined')] Player joined: ${data.userId}`);
     updatePlayersList(data.names);
 });
 
-startGameButton.addEventListener('click', () => {
+startGameButton.addEventListener("click", () => {
     const gameCode = waitingRoomGameCode.textContent.trim();
     console.log(`[script.js/startGameButton.click] Starting game for game code: ${gameCode}`);
-    socket.emit('startGame', { gameCode });
+    socket.emit("startGame", { gameCode });
 });
 
-socket.on('startGame', (data) => {
+socket.on("startGame", (data) => {
     console.log(`[script.js/socket.on('startGame')] Starting game for game code: ${data.gameCode}`);
-    waitingRoomDiv.style.display = 'none';
-    gameDiv.style.display = 'flex';
+    waitingRoomDiv.style.display = "none";
+    gameDiv.style.display = "flex";
 
     currentRankingIndex = 0;
     rankings = data.names.map((name, index) => ({
         name,
-        rank: index + 1
+        rank: index + 1,
     }));
 
-    topLabel.textContent = 'Dirty';
-    bottomLabel.textContent = 'Clean';
+    const topCriteria = data.rankingCriteria[1];
+    const bottomCriteria = data.rankingCriteria[2];
 
-    sortableList.innerHTML = '';
+    topLabel.textContent = topCriteria;
+    bottomLabel.textContent = bottomCriteria;
+
+    sortableList.innerHTML = "";
     rankings.forEach(({ name }) => {
-        const li = document.createElement('li');
+        const li = document.createElement("li");
         li.textContent = name;
         sortableList.appendChild(li);
     });
-
-    const gameCode = waitingRoomGameCode.textContent.trim();
-    socket.emit('updateRankings', { gameCode, userId, rankings });
 
     new Sortable(sortableList, {
         animation: 150,
@@ -155,69 +155,82 @@ socket.on('startGame', (data) => {
             }));
             console.log(`[script.js] Updated Rankings: ${JSON.stringify(updatedRankings)}`);
 
-            // Update rankings in Firebase
-            socket.emit('updateRankings', { gameCode, userId, rankings: updatedRankings });
+            const gameCode = waitingRoomGameCode.textContent.trim();
+            socket.emit("updateRankings", { gameCode, userId, rankings: updatedRankings });
         }
     });
+
+    const gameCode = waitingRoomGameCode.textContent.trim();
+    socket.emit("updateRankings", { gameCode, userId, rankings });
 });
 
-lockButton.addEventListener('click', () => {
+lockButton.addEventListener("click", () => {
     const rankings = Array.from(sortableList.children).map((li, index) => ({
         name: li.textContent,
-        rank: index + 1
+        rank: index + 1,
     }));
     const gameCode = waitingRoomGameCode.textContent.trim();
 
     console.log(`[script.js/lockButton.click] Rankings locked in: ${JSON.stringify(rankings)}`);
 
-    socket.emit('submitRankings', { gameCode, userId, rankings });
+    socket.emit("submitRankings", { gameCode, userId, rankings });
 
-    gameDiv.style.display = 'none';
-    resultDiv.style.display = 'flex';
-    resultText.textContent = 'Waiting for all votes...';
+    gameDiv.style.display = "none";
+    resultDiv.style.display = "flex";
+    resultText.textContent = "Waiting for all votes...";
+    finalResultsList.innerHTML = ""; // Ensure final results are hidden until all votes are in
 });
 
-socket.on('displayFinalResults', (data) => {
+socket.on("displayFinalResults", (data) => {
     console.log(`[script.js/socket.on('displayFinalResults')] Displaying final results`);
-    resultText.textContent = 'Final Rankings';
-    finalResultsList.innerHTML = '';
-    data.finalResults.forEach(result => {
-        const li = document.createElement('li');
+    resultText.textContent = "Final Rankings";
+
+    const topCriteria = data.rankingCriteria ? data.rankingCriteria[1] : "Top";
+    const bottomCriteria = data.rankingCriteria ? data.rankingCriteria[2] : "Bottom";
+
+    topLabel.textContent = topCriteria;
+    bottomLabel.textContent = bottomCriteria;
+
+    finalResultsList.innerHTML = "";
+    data.finalResults.forEach((result) => {
+        const li = document.createElement("li");
         li.textContent = `${result.name} (${result.rank})`;
         finalResultsList.appendChild(li);
     });
+    nextButton.style.display = localStorage.getItem("isCreator") === "true" ? "block" : "none";
 });
 
-nextButton.addEventListener('click', () => {
-    currentRankingIndex++;
+nextButton.addEventListener("click", () => {
     const gameCode = waitingRoomGameCode.textContent.trim();
-    if (currentRankingIndex < rankings.length) {
-        socket.emit('nextRanking', { gameCode, rankingId: currentRankingIndex + 1 });
-    } else {
-        resultText.textContent = 'Game Over';
-        nextButton.style.display = 'none';
-    }
+    console.log(`[script.js/nextButton.click] Moving to next ranking for game code: ${gameCode}`);
+    socket.emit("nextRanking", { gameCode });
 });
 
-socket.on('showRanking', (data) => {
-    resultDiv.style.display = 'none';
-    gameDiv.style.display = 'flex';
-    showRanking();
+socket.on("refreshRanking", (data) => {
+    console.log(`[script.js/socket.on('refreshRanking')] Refreshing rankings for new round`);
+
+    gameDiv.style.display = "flex";
+    resultDiv.style.display = "none";
+
+    const topCriteria = data.rankingCriteria[1];
+    const bottomCriteria = data.rankingCriteria[2];
+
+    topLabel.textContent = topCriteria;
+    bottomLabel.textContent = bottomCriteria;
+
+    sortableList.innerHTML = "";
+    data.names.forEach((name) => {
+        const li = document.createElement("li");
+        li.textContent = name;
+        sortableList.appendChild(li);
+    });
 });
 
 function updatePlayersList(names) {
-    playersList.innerHTML = '';
-    names.forEach(name => {
-        const li = document.createElement('li');
+    playersList.innerHTML = "";
+    names.forEach((name) => {
+        const li = document.createElement("li");
         li.textContent = name;
         playersList.appendChild(li);
     });
-}
-
-function showRanking() {
-    if (currentRankingIndex < rankings.length) {
-        const ranking = rankings[currentRankingIndex];
-        topLabel.textContent = `Round ${currentRankingIndex}`;
-        bottomLabel.textContent = `${ranking[1]} vs ${ranking[2]}`;
-    }
 }
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const path = require('path');
-const admin = require('firebase-admin');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require("path");
+const admin = require("firebase-admin");
 
 const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://slider-3b16e-default-rtdb.firebaseio.com"
+    databaseURL: "https://slider-3b16e-default-rtdb.firebaseio.com",
 });
 
 const app = express();
@@ -17,74 +17,97 @@ const io = socketIo(server);
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-io.on('connection', (socket) => {
-    console.log('[server.js/io.on(connection)] A user connected');
+io.on("connection", (socket) => {
+    console.log("[server.js/io.on(connection)] A user connected");
 
-    socket.on('createGame', ({ gameCode, names, userId }) => {
-        console.log(`[server.js/socket.on('createGame')] Game code: ${gameCode}, Names: ${names.join(', ')}, UUID: ${userId}`);
+    socket.on("createGame", ({ gameCode, names, userId }) => {
+        console.log(`[server.js/socket.on('createGame')] Game code: ${gameCode}, Names: ${names.join(", ")}, UUID: ${userId}`);
 
         const db = admin.database();
         const gameRef = db.ref(`games/${gameCode}`);
-        gameRef.set({ names, creator: userId, playersCount: 1, completed: 0 });
+        gameRef.set({ names, creator: userId, playersCount: 1, completed: 0, version: 0, state: "waiting" });
 
         socket.join(gameCode);
-        io.to(gameCode).emit('gameJoined', { gameCode, names });
+        io.to(gameCode).emit("gameJoined", { gameCode, names });
     });
 
-    socket.on('joinGame', ({ gameCode, userId }) => {
+    socket.on("joinGame", ({ gameCode, userId }) => {
         console.log(`[server.js/socket.on('joinGame')] Game code: ${gameCode}, UUID: ${userId}`);
 
         const db = admin.database();
         const gameRef = db.ref(`games/${gameCode}`);
-        gameRef.once('value', (snapshot) => {
+        gameRef.once("value", (snapshot) => {
             const gameData = snapshot.val() || {};
-            gameRef.child('playersCount').transaction(count => (count || 0) + 1);
+            gameRef.child("playersCount").transaction((count) => (count || 0) + 1);
             socket.join(gameCode);
-            io.to(gameCode).emit('playerJoined', { userId, names: gameData.names });
+            io.to(gameCode).emit("playerJoined", { userId, names: gameData.names });
+
+            if (gameData.state === "waiting") {
+                gameRef.child("state").set("voting");
+                console.log(`[server.js/joinGame] Game state set to voting for game code: ${gameCode}`);
+
+                const version = gameData.version || 0;
+                const rankingsRef = db.ref('rankings');
+                rankingsRef.once("value", (rankingsSnapshot) => {
+                    const allRankings = rankingsSnapshot.val() || [];
+                    const rankingCriteria = allRankings[version];
+                    io.to(gameCode).emit("startGame", { gameCode, names: gameData.names, rankingCriteria });
+                });
+            }
         });
     });
 
-    socket.on('checkGameExists', ({ gameCode, userId }) => {
+    socket.on("checkGameExists", ({ gameCode, userId }) => {
         console.log(`[server.js/socket.on('checkGameExists')] Checking if game code ${gameCode} exists`);
 
         const db = admin.database();
         const gameRef = db.ref(`games/${gameCode}`);
 
-        gameRef.once('value', (snapshot) => {
+        gameRef.once("value", (snapshot) => {
             const exists = snapshot.exists();
             let userExists = false;
             if (exists) {
-                const players = snapshot.child('players').val() || {};
+                const players = snapshot.child("players").val() || {};
                 userExists = players.hasOwnProperty(userId);
             }
-            socket.emit('gameExists', { exists, userExists, gameCode, userId });
+            socket.emit("gameExists", { exists, userExists, gameCode, userId });
         });
     });
 
-    socket.on('startGame', ({ gameCode }) => {
+    socket.on("startGame", ({ gameCode }) => {
         console.log(`[server.js/socket.on('startGame')] Starting game for game code: ${gameCode}`);
 
         const db = admin.database();
         const gameRef = db.ref(`games/${gameCode}`);
 
-        gameRef.once('value', (gameSnapshot) => {
+        gameRef.once("value", (gameSnapshot) => {
             const gameData = gameSnapshot.val() || {};
-            const initialRankings = gameData.names.map((name, index) => ({
-                name,
-                rank: index + 1
-            }));
+            const version = gameData.version || 0;
 
-            for (const playerId of Object.keys(gameData.players || {})) {
-                db.ref(`games/${gameCode}/rankings/${playerId}`).set(initialRankings);
-            }
+            const rankingsRef = db.ref('rankings');
+            rankingsRef.once("value", (rankingsSnapshot) => {
+                const allRankings = rankingsSnapshot.val() || [];
+                const rankingCriteria = allRankings[version];
 
-            io.to(gameCode).emit('startGame', { gameCode, names: gameData.names });
+                const initialRankings = gameData.names.map((name, index) => ({
+                    name,
+                    rank: index + 1,
+                }));
+
+                for (const playerId of Object.keys(gameData.players || {})) {
+                    db.ref(`games/${gameCode}/rankings/${playerId}`).set(initialRankings);
+                }
+
+                gameRef.child("state").set("voting");
+                console.log(`[server.js/startGame] Game state set to voting for game code: ${gameCode}`);
+                io.to(gameCode).emit("startGame", { gameCode, names: gameData.names, rankingCriteria });
+            });
         });
     });
 
-    socket.on('updateRankings', ({ gameCode, userId, rankings }) => {
+    socket.on("updateRankings", ({ gameCode, userId, rankings }) => {
         console.log(`[server.js/socket.on('updateRankings')] Game code: ${gameCode}, User ID: ${userId}, Rankings: ${JSON.stringify(rankings)}`);
 
         const db = admin.database();
@@ -93,25 +116,22 @@ io.on('connection', (socket) => {
         userRankingsRef.set(rankings);
     });
 
-    socket.on('submitRankings', ({ gameCode, userId, rankings }) => {
+    socket.on("submitRankings", ({ gameCode, userId, rankings }) => {
         console.log(`[server.js/socket.on('submitRankings')] Game code: ${gameCode}, User ID: ${userId}, Rankings: ${JSON.stringify(rankings)}`);
 
         const db = admin.database();
         const gameRef = db.ref(`games/${gameCode}`);
         const userRankingsRef = gameRef.child(`rankings/${userId}`);
-        const completedRef = gameRef.child('completed');
+        const completedRef = gameRef.child("completed");
 
-        // Update user's rankings
         userRankingsRef.set(rankings);
 
-        // Increment the number of completed rankings
-        completedRef.transaction(current => (current || 0) + 1);
+        completedRef.transaction((current) => (current || 0) + 1);
 
-        // Check if all players have submitted their rankings
-        gameRef.once('value', snapshot => {
+        gameRef.once("value", (snapshot) => {
             const gameData = snapshot.val();
             const playerCount = gameData.playersCount;
-            completedRef.once('value', compSnap => {
+            completedRef.once("value", (compSnap) => {
                 if (compSnap.val() === playerCount) {
                     const allRankings = gameData.rankings;
                     const finalScores = {};
@@ -132,30 +152,64 @@ io.on('connection', (socket) => {
                         .sort((a, b) => a.score - b.score);
 
                     const highestScore = sortedResults[0].score;
-                    const finalResults = sortedResults.map((item) => ({
+                    const finalResults = sortedResults.map((item, index) => ({
                         ...item,
-                        rank: item.score === highestScore ? 'Tied for 1' : `Rank ${sortedResults.indexOf(item) + 1}`
+                        rank: index === 0 ? `Rank 1` : `Rank ${index + 1}`,
                     }));
 
-                    io.to(gameCode).emit('displayFinalResults', { finalResults });
+                    // Clear user rankings
+                    for (const playerId of Object.keys(gameData.rankings)) {
+                        db.ref(`games/${gameCode}/rankings/${playerId}`).remove();
+                    }
+
+                    // Reset completed count
+                    gameRef.child("completed").set(0);
+                    gameRef.child("state").set("waiting");
+                    console.log(`[server.js/submitRankings] Game state set to waiting for game code: ${gameCode}`);
+
+                    io.to(gameCode).emit("displayFinalResults", { finalResults, rankingCriteria: gameData.rankingCriteria });
                 }
             });
         });
     });
 
-    socket.on('nextRanking', ({ gameCode, rankingId }) => {
-        console.log(`[server.js/socket.on('nextRanking')] Next ranking for game code: ${gameCode}, Ranking ID: ${rankingId}`);
+    socket.on("nextRanking", ({ gameCode }) => {
+        console.log(`[server.js/socket.on('nextRanking')] Next ranking for game code: ${gameCode}`);
 
         const db = admin.database();
         const gameRef = db.ref(`games/${gameCode}`);
-        gameRef.once('value', (snapshot) => {
+
+        gameRef.once("value", (snapshot) => {
             const gameData = snapshot.val() || {};
-            io.to(gameCode).emit('showRanking', { gameCode, rankings: gameData.rankings });
+            const newVersion = (gameData.version || 0) + 1;
+
+            gameRef.child("version").set(newVersion);
+            gameRef.child("completed").set(0);
+            gameRef.child("rankings").remove();
+
+            const rankingsRef = db.ref('rankings');
+            rankingsRef.once("value", (rankingsSnapshot) => {
+                const allRankings = rankingsSnapshot.val() || [];
+                const rankingCriteria = allRankings[newVersion];
+                const names = gameData.names;
+                const initialRankings = names.map((name, index) => ({
+                    name,
+                    rank: index + 1,
+                }));
+
+                for (const playerId of Object.keys(gameData.players || {})) {
+                    db.ref(`games/${gameCode}/rankings/${playerId}`).set(initialRankings);
+                }
+
+                gameRef.child("state").set("voting");
+                console.log(`[server.js/nextRanking] Game state set to voting for game code: ${gameCode}`);
+                io.to(gameCode).emit("refreshRanking", { names, rankingCriteria });
+            });
         });
     });
 
-    socket.on('disconnect', () => {
-        console.log('[server.js/io.on(disconnect)] A user disconnected');
+    socket.on("disconnect", () => {
+        console.log("[server.js/io.on(disconnect)] A user disconnected");
     });
 });
 
